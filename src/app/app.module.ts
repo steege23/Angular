122ef2dcfef757bcbdfe8d7b905ca5081903a3d4
steege23 <lennarttersteege23@hotmail.com>
2019-06-03 14:13:22 +0200

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { CartComponent } from './cart/cart.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ShippingComponent } from './shipping/shipping.component';
 
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-     { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+       { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -28,9 +32,11 @@ import { ReactiveFormsModule } from '@angular/forms';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   providers: [CartService]
+  
 })
 export class AppModule { }
 
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
+
+  constructor(
+    private http: HttpClient)
+    {}
+
   addToCart(product) {
     this.items.push(product);
   }
@@ -16,5 +22,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
   }
 }
